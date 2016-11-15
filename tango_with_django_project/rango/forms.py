b894ae99fae6148910d87444d1856ffff636aaea
admly <admly@interia.pl>
2016-11-15 14:56:20 +0100

    Finished chapter 9. Exercises left.

--- tango_with_django_project/rango/forms.py
+++ tango_with_django_project/rango/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from rango.models import Page, Category
+from django.contrib.auth.models import User
+from rango.models import Category, Page, UserProfile
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=128,
@@ -22,7 +23,7 @@ class PageForm(forms.ModelForm):
     help_text="Please enter the URL of the page.")
 
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-    
+
     def clean(self):
         cleaned_data = self.cleaned_data
         url = cleaned_data.get('url')
@@ -46,3 +47,12 @@ class PageForm(forms.ModelForm):
         exclude = ('category',)
         # or specify the fields to include (i.e. not include the category field)
         #fields = ('title', 'url', 'views')
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture')
